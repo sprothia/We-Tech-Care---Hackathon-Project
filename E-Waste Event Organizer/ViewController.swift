1c2a2db51a179da37a838748d1e04408819fa698
Siddharth Prothia <sprothia935@gmail.com>
2023-09-08 21:42:55 -0700

    Initial Commit

---
+++ E-Waste Event Organizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-Waste Event Organizer
+//
+//  Created by Mac on 9/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
